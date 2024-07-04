2c6930fcbb14f13391c11b6ade9094379f4e94bb
João-NB <nbjoaoguilherme@gmail.com>
2024-07-03 23:23:22 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home": "url('/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
